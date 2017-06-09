87d848750d0552d8ee0c703d74c6939938052320
arzyu <arzyu@live.cn>
2017-06-10 00:34:37 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import * as child_process from 'child_process';
+import * as path from 'path';
+
+import * as program from 'commander';
+
+import * as packageInfo from '../package.json';
+import run from './tasks';
+
+function resolvePath(jekyllSite: string): string {
+  return path.resolve(process.cwd(), jekyllSite);
+}
+
+program
+  .version((packageInfo as any).version)
+  .option('--jekyll-site <path>', 'Jekyll site path', resolvePath, process.cwd())
+  .option('--port <port>', 'Livereload server port', '4000')
+  .parse(process.argv);
+
+run({
+  jekyllPath: program.jekyllSite,
+  port: program.port
+});
---
+++ src/tasks.ts
@@ -0,0 +1,68 @@
+import * as child_process from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import * as gulp from 'gulp';
+import * as server from 'gulp-server-livereload';
+import * as yaml from 'js-yaml';
+
+export default function run(options: {jekyllPath: string, port: string}) {
+  const { jekyllPath, port } = options;
+
+  function buildJekyll(): void {
+    const cmd = 'bundle exec jekyll build --incremental && echo';
+    try {
+      child_process.execSync(cmd, { cwd: jekyllPath, stdio: 'inherit' });
+    } catch (error) {} // tslint:disable-line:no-empty
+  }
+
+  function getJekyllTheme(): string {
+    try {
+      const configYaml = fs.readFileSync(`${jekyllPath}/_config.yml`, 'utf8');
+      const jekyllConfig: object = yaml.safeLoad(configYaml);
+      const themeName = (jekyllConfig as any).theme;
+      const themePath = child_process.execSync(
+        `bundle show ${themeName} 2>/dev/null`,
+        { cwd: jekyllPath, encoding: 'utf8' }
+      );
+      return themePath.trim();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const themePath = getJekyllTheme();
+
+  gulp.task('watch', ['build'], () => {
+    const globs = [
+      `${themePath}/**/*`,
+      `${jekyllPath}/**/*`,
+      `!${jekyllPath}/_site/**/*`
+    ];
+    gulp.watch(globs, (event) => {
+      const parentPath = path.resolve(
+        event.path.startsWith(themePath) ? themePath : jekyllPath,
+        '..'
+      );
+      const relativePath = path.relative(parentPath, event.path);
+      console.log(`\n[jekyll-dev] ${relativePath}, ${event.type}\n`);
+      buildJekyll();
+    });
+  });
+
+  gulp.task('server', ['build'], () => {
+    gulp.src(`${jekyllPath}/_site`)
+      .pipe(server({ livereload: true, port }));
+  });
+
+  gulp.task('build', (done) => {
+    buildJekyll();
+    done();
+  });
+
+  gulp.task('default', ['watch', 'server']);
+
+  console.log(`\n[jekyll-dev]:\n    site => ${jekyllPath}\n   theme => ${themePath}\n`);
+
+  gulp.start();
+}
